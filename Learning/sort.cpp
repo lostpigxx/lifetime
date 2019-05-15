0fb2151e9a9b4be2bc65b88ec190f93085a7e676
xiao.liang <lxlxwana@gmail.com>
2019-05-15 14:22:38 +0800

    add many sort algorithm

--- Learning/sort.cpp
+++ Learning/sort.cpp
@@ -1,10 +1,253 @@
 #include <iostream>
 #include <vector>
 #include <random>
-
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
+
+void print(const vector<int> v) {
+    for (auto val : v) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+vector<int> Sort_stl(const vector<int>& array) {
+    vector<int> v(array);
+    sort(v.begin(), v.end());
+    return v;
+}
+
+
+vector<int> Sort_select(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+    
+    for (int i = 0; i < n; ++i) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (v[j] < v[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(v[i], v[min_idx]);
+    }
+    return v;
+}
+
+vector<int> Sort_bubble(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    for (int i = n - 1; i > 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            if (v[j] > v[j+1])
+                swap(v[j], v[j+1]);
+        }
+    }
+
+    return v;
+}
+
+vector<int> Sort_insert(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    for (int i = 1; i < n; ++i) {
+        int cur = v[i];
+        int j;
+        for (j = i - 1; j >= 0; --j) {
+            if (v[j] > cur) {
+                v[j+1] = v[j];
+            }
+            else {
+                v[j+1] = cur;
+                break;
+            }
+        }
+        if (j == -1)
+            v[0] = cur;
+    }
+
+    return v;
+}
+
+
+
+vector<int> Sort_merge(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    if (n < 2)
+        return v;
+    
+    int mid = n / 2;
+    vector<int> left(Sort_merge(vector<int>(v.begin(), v.begin() + mid)));
+    vector<int> right(Sort_merge(vector<int>(v.begin() + mid, v.end())));
+
+    v.clear();
+    // merge
+    int i = 0, j = 0;
+    while (i < left.size() && j < right.size()) {
+        if (left[i] < right[j])
+            v.push_back(left[i++]);
+        else 
+            v.push_back(right[j++]);
+    }
+    while (i < left.size()) {
+        v.push_back(left[i++]);
+    }
+    while (j < right.size()) {
+        v.push_back(right[j++]);
+    }
+
+    return v;
+}
+
+
+int partition(vector<int>& v, int left, int right) {
+    int pivot = left;
+    int mid = left + 1;
+
+    for (int i = mid; i < right; ++i) {
+        if (v[i] < v[pivot]) {
+            swap(v[i], v[mid++]);
+        }
+    }
+    --mid;
+    swap(v[pivot], v[mid]);
+    return mid;
+}
+
+void quick_sort(vector<int>& v, int left, int right) {
+    int n = right - left;
+    if (n < 2)
+        return;
+    
+    int mid = partition(v, left, right);
+    quick_sort(v, left, mid + 1);
+    quick_sort(v, mid+1, right);
+
+}
+
+vector<int> Sort_quick(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    quick_sort(v, 0, n);
+
+    return v;
+}
+
+vector<int> Sort_heap_stl(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+    vector<int> ans;
+
+    make_heap(v.begin(), v.end(), greater<int>());
+    for (int i = 0; i < n; ++i) {
+        ans.push_back(v[0]);
+        pop_heap(v.begin(), v.begin() + n - i, greater<int>());
+    }
+
+    return ans;
+}
+
+vector<int> Sort_heap_priority_queue(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    priority_queue<int, vector<int>, greater<int>> pq(v.begin(), v.end());
+    v.clear();
+    while (!pq.empty()) {
+        v.push_back(pq.top());
+        pq.pop();
+    }
+
+    return v;
+}
+
+void Adjust_max_heap(vector<int>& v, int i, int n) {
+    int left = i * 2 + 1;
+    int right = i * 2 + 2;
+    int max = i;
+
+    if (left < n && v[left] > v[max]) {
+        max = left;
+    }
+
+    if (right < n && v[right] > v[max]) {
+        max = right;
+    }
+
+    if (max != i) {
+        swap(v[i], v[max]);
+        Adjust_max_heap(v, max, n);
+    }
+}
+
+
+void Build_max_heap(vector<int>& v) {
+    int n = v.size();
+
+    for (int i = n-1; i >= 0; --i) {
+        Adjust_max_heap(v, i, n);
+    }
+}
+
+
+vector<int> Sort_heap(const vector<int>& array) {
+    vector<int> v(array);
+    int n = v.size();
+
+    Build_max_heap(v);
+    for (int i = 0; i < n; ++i) {
+        swap(v[0], v[n-1-i]);
+        Adjust_max_heap(v, 0, n - i - 1);
+    }
+
+    return v;
+}
+
+
 int main() {
 
+    int n = 100;
+    vector<int> array;
+
+    // generate random number
+    default_random_engine rnd(time(nullptr));
+    uniform_int_distribution<int> uni(0, n*10);
+    for (int i = 0; i < n; ++i) {
+        array.push_back(uni(rnd));
+    }
+
+    print(Sort_stl(array));
+    cout << endl;
+
+    print(Sort_select(array));
+    cout << endl;
+
+    print(Sort_bubble(array));
+    cout << endl;
+
+    print(Sort_insert(array));
+    cout << endl;
+
+    print(Sort_merge(array));
+    cout << endl;
+
+    print(Sort_quick(array));
+    cout << endl;
+
+    print(Sort_heap_stl(array));
+    cout << endl;
+
+    print(Sort_heap_priority_queue(array));
+    cout << endl;
+
+    print(Sort_heap(array));
+    cout << endl;
 }
--- Learning/topK.cpp
+++ Learning/topK.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
 #include <vector>
+#include <random>
 
 using namespace std;
+
+
+class TopK {
+  public:
+    vector<int> TopK_sort(const vector<int>& v, int k);
+    vector<int> TopK_partial_sort(const vector<int>& v, int k);
+    vector<int> TopK_heap(const vector<int>& v, int k);
+    vector<int> TopK_random_select(const vector<int>& v, int k);
+    vector<int> TopK_stl_nth_element(const vector<int>& v, int k);
+
+};
+
 
 void print(vector<int> ans) {
     for (auto val : ans) {
@@ -11,49 +23,143 @@ void print(vector<int> ans) {
     cout << endl;
 }
 
+bool cmp(const int& a, const int& b) {
+    return a < b;
+}
 
-int partition(int l, int r, vector<int>& array) {
-    int pivot = array[l];
-    int i = l + 1, j = r;
-    while (true) {
-        while (array[i] < pivot && i <= r) i++;
-        while (array[j] > pivot && j >= l) j--;
-        if (i < j)
-            swap(array[i++], array[j++]);
-        else 
-            break;
+// int partition(int l, int r, vector<int>& array) {
+//     int pivot = array[l];
+//     int i = l + 1, j = r;
+//     while (true) {
+//         while (array[i] < pivot && i <= r) i++;
+//         while (array[j] > pivot && j >= l) j--;
+//         if (i < j)
+//             swap(array[i++], array[j++]);
+//         else 
+//             break;
+//     }
+//     return j;
+// }
+
+// void topK(int l, int r, int K, vector<int>& array) {
+//     print(array);
+
+//     int mid = partition(l, r, array);
+//     int left_length = mid - l + 1;
+//     if (left_length < K)
+//         topK(mid+1, r, K - left_length, array);
+//     else if (left_length > K)
+//         topK(l, mid, K, array);
+// }
+
+
+// vector<int> TopK_RandomSelect(vector<int>& array, int K) {
+//     vector<int> ans;
+    
+//     topK(0, array.size(), K, array);
+    
+//     for (int i = 0; i < K; i++)
+//         ans.push_back(array[i]);
+//     return ans; 
+
+// }
+
+// time: O(n) expected
+vector<int> TopK::TopK_random_select(const vector<int>& v, int k) {
+    vector<int> tmp(v);
+
+    
+
+    return vector<int>(tmp.begin(), tmp.begin() + k);;
+}
+
+// time: O(nlogn)
+vector<int> TopK::TopK_sort(const vector<int>& v, int k) {
+    vector<int> tmp(v);
+
+    sort(tmp.begin(), tmp.end());    
+
+    return vector<int>(tmp.begin(), tmp.begin() + k);;
+}
+
+// time: O(klogn)
+vector<int> TopK::TopK_heap(const vector<int>& v, int k) {
+    vector<int> tmp(v);
+    
+    make_heap(tmp.begin(), tmp.begin() + k);
+    for (int i = k; i < tmp.size(); i++) {
+        if (tmp[i] < tmp[0]) {
+            pop_heap(tmp.begin(), tmp.begin() + k);
+            swap(tmp[k-1], tmp[i]);
+            push_heap(tmp.begin(), tmp.begin() + k);
+        }
     }
-    return j;
+
+    return vector<int>(tmp.begin(), tmp.begin() + k);;
 }
 
-void topK(int l, int r, int K, vector<int>& array) {
-    print(array);
+// time: O(n) expected
+vector<int> TopK::TopK_stl_nth_element(const vector<int>& v, int k) {
+    vector<int> tmp(v);
 
-    int mid = partition(l, r, array);
-    int left_length = mid - l + 1;
-    if (left_length < K)
-        topK(mid+1, r, K - left_length, array);
-    else if (left_length > K)
-        topK(l, mid, K, array);
+    nth_element(tmp.begin(), tmp.begin() + k, tmp.end());
+
+    return vector<int>(tmp.begin(), tmp.begin() + k);
 }
 
+// time: O(k * n)
+vector<int> TopK::TopK_partial_sort(const vector<int>& v, int k) {
+    vector<int> tmp(v);
 
-vector<int> TopK_RandomSelect(vector<int>& array, int K) {
-    vector<int> ans;
-    
-    topK(0, array.size(), K, array);
-    
-    for (int i = 0; i < K; i++)
-        ans.push_back(array[i]);
-    return ans; 
+    for (int i = 0; i < k; i++) {
+        int min = i;
+        for (int j = i + 1; j < tmp.size(); j++) {
+            if (tmp[j] < tmp[min])
+                min = j;
+        }
+        swap(tmp[i], tmp[min]);
+    }
 
+    return vector<int>(tmp.begin(), tmp.begin() + k);
 }
 
 int main() {
-    vector<int> array = {4, 21, 56, 14, 90, 24, 18, 1};
+    TopK solution;
+
+    int n, k;
+    n = 100;
+    k = 10;
+
+    vector<int> array;
+
+    // generate random number
+    default_random_engine rnd(time(nullptr));
+    uniform_int_distribution<int> uni(0, n*10);
+    for (int i = 0; i < n; ++i) {
+        array.push_back(uni(rnd));
+    }
+
+    vector<int> ans0 = solution.TopK_stl_nth_element(array, k);
+    vector<int> ans1 = solution.TopK_sort(array, k);
+    vector<int> ans2 = solution.TopK_heap(array, k);
+    vector<int> ans3 = solution.TopK_partial_sort(array, k);
 
     print(array);
-    vector<int> ans = TopK_RandomSelect(array, 3);
+    cout << endl;
+
+    sort(ans0.begin(), ans0.end());
+    print(ans0);
+    cout << endl;
     
+    sort(ans1.begin(), ans1.end());
+    print(ans1);
+    cout << endl;
+    
+    sort(ans2.begin(), ans2.end());
+    print(ans2);
+    cout << endl;
+
+    sort(ans3.begin(), ans3.end());
+    print(ans3);
 
 }
